c3c903111ab6e09ae225e82f40242268ee08004d
ava_kyoko <80278345+ava8kyoko@users.noreply.github.com>
2022-03-28 13:57:06 -0400

    code ok

--- mod37.c
+++ mod37.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 int		main(void)
 {
 	int		numbers[22] = {202, 137, 390, 235, 114, 369, 198, 110, 350, 396, 390, 383, 225, 258, 38, 291, 75, 324, 401, 142, 288, 397};
@@ -15,20 +17,21 @@ int		main(void)
 	while (++i < 22)
 	{
 		converted[i] = numbers[i] % modulus;
-		printf("%d ", converted[i]);
 		if (converted[i] >= 0 && converted[i] <= 25) // ASCII 65 - 0
 			converted[i] += 65;
 		else if (converted[i] >= 26 && converted[i] <= 35) // ASCII 48 - 26
 			converted[i] += 22;
 		else if (converted[i] == 36)
 			converted[i] += 59; // ASCII 95 - 36
+		printf("%d ", converted[i]);
 	}
 	printf("\n");
 
 	i = -1;
+	printf("picoCTF{");
 	while (++i < 22)
 		printf("%c", converted[i]);
-	printf("\n");
+	printf("}\n");
 
 	return (0);
 }
@@ -37,6 +40,6 @@ int		main(void)
 gcc mod37.c
 ./a.out    
 numbers : 202 137 390 235 114 369 198 110 350 396 390 383 225 258 38 291 75 324 401 142 288 397 
-converted : 17 26 20 13 3 36 13 36 17 26 20 13 3 36 1 32 1 28 31 31 29 27 
+converted : 82 48 85 78 68 95 78 95 82 48 85 78 68 95 66 54 66 50 53 53 51 49 
 picoCTF{R0UND_N_R0UND_B6B25531}
 **/
